9c7bf6446b1d280d9123884345ac1e7be25c14f9
VladMSI <vladpovorin@gmail.com>
2024-04-11 14:22:21 +0300

    Сделано. 1 задача

--- Lesson_11_04/src/Main.java
+++ Lesson_11_04/src/Main.java
@@ -1,5 +1,50 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        int N = 0;
+        boolean isValid = false;
+
+        do {
+            try {
+                System.out.print("Введите колличество сопротивлений N = ");
+                N = sc.nextInt();
+                if (N <= 0) {
+                    throw new IllegalArgumentException("Значение должно быть больше нуля");
+                }
+                isValid = true;
+            } catch (Exception e) {
+                System.out.println("Ошибка: " + e.getMessage());
+                sc.nextLine(); // очистить буфер ввода
+            }
+        } while (!isValid);
+        boolean isInvalid = false;
+        double re_sum = 0;
+        int R = 0;
+        for (int i = 0; i < N; i++) {
+            do {
+                try {
+                    System.out.print("Введите значене в Омах для "+(i+1)+"-го сопротивления R["+(i+1)+"] = ");
+                    R = sc.nextInt();
+                    if (R <= 0) {
+                        throw new IllegalArgumentException("Значение должно быть больше нуля");
+                    }
+                    isInvalid = false;
+                } catch (Exception e) {
+                    System.out.println("Ошибка: " + e.getMessage());
+                    sc.nextLine(); // очистить буфер ввода
+                    isInvalid = true;
+                }
+            } while (isInvalid);
+
+            re_sum += (double) 1 /R;
+        }
+
+
+        String res = String.format("%.2f", (1/re_sum));
+        System.out.print("Значение сопротивления цепи: " + res +" ОМ");
+
+
     }
 }
